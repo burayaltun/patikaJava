0411b40a2a60267ca2a777c6675b91a0f4b06c54
Berke <adilaltun@adil-MacBook-Air.local>
2023-02-17 15:54:21 +0300

    commit

---
+++ ödev2.java
@@ -0,0 +1,28 @@
+public class ödev2 {
+    import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner inputPrice = new Scanner(System.in);
+        System.out.print("Tutar giriniz: ");
+
+        double tutar= inputPrice.nextDouble();
+
+        
+
+        double kdv= (tutar < 1000) ? 0.18 : 0.08;
+        double kdvDeger = kdv * tutar;
+        double toplamTutar =tutar + kdvDeger;
+        inputPrice.close();
+
+        System.out.println("Tutar: " + tutar);
+        System.out.println("KDV oranı: " + kdv);
+        System.out.println("KDV değeri: " + kdvDeger);
+        System.out.println("Toplam tutar: " + toplamTutar);
+
+    }
+}
+}
